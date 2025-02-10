b1e21aafc278a1e35cf58d1e162dcf03017d972f
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2025-02-10 22:45:02 +0530

    Add function for creating and storing book object

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Odin Library");
+const myLibrary = [];
+
+function Book(title, author, noOfPages, hasRead) {
+
+	// Check if the function is called as a constructor function using the new operator
+	if(!new.target) {
+		throw Error("Must use the new operator to call the function");
+	}
+
+	this.title = title;
+	this.author = author;
+	this.noOfPages = noOfPages;
+	this.hasRead = hasRead;
+}
+
+function addBookToLibrary(title, author, noOfPages, hasRead) {
+  
+	const book = new Book(title, author, noOfPages, hasRead);
+	
+	myLibrary.push(book);
+}
